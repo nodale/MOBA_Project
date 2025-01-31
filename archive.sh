7827fb622be8836fedc58a436ddd755dfa4c49e5
Daniels <d.vidulejs@tum.de>
2025-01-31 11:42:00 +0100

    fix archive script

--- archive.sh
+++ archive.sh
@@ -1,4 +1,4 @@
 # git garbage collector reduce .git size
 git -c gc.reflogExpire=0 -c gc.reflogExpireUnreachable=0 -c gc.rerereresolved=0 -c gc.rerereunresolved=0 -c gc.pruneExpire=now gc "$@"
-tar --exclude='.venv' --exclude='backward_facing_step_compressible/*/*' --exclude='backward_facing_step_compressible/[1-9]*' --exclude='*.tar.gz' --exclude='*.tar' -cvf archive.tar * .git
+tar --exclude='.venv' --exclude='backward_facing_step_compressible/[1-9]*' --exclude='*.tar.gz' --exclude='*.tar' -cvf archive.tar * .git
 # extract using tar -xvf archive.tar
